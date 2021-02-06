47fbef4aa0f4c55e6bf70f5b99139a4d0b2ccd1b
Melinda <melindaeudy@gmail.com>
2021-02-06 15:20:29 -0500

    final version of code

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "00af5e4d292309ab2481d0925195c95f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB_UpzLko_gf_KJNj9tzfHwKsMSZUIGsms"
